a3e7dcc575465eaa55055d5605a94053aa0ce8dd
Noel Vaca Moreno <noel.vm.2501@gmail.com>
2017-12-26 22:04:08 -0400

    Actualización de lib

--- MediumAPI/app/src/main/java/com/anncode/mediumapi/mediumApi/Constants.java
+++ MediumAPI/app/src/main/java/com/anncode/mediumapi/mediumApi/Constants.java
@@ -8,5 +8,5 @@ public class Constants {
     public static final String VERSION = "/v1/";
     public static final String ROOT_URL = "https://api.medium.com" + VERSION;
     public static final String URL_GET_USER = "me/";
-    public static final String BEARER = "224d98ec9e3f37dcc7fc0f52fa17a4d86b8c8d28c164916ba05cea8ecb08d926a";
+    public static final String BEARER = "2ff139226208e91bd0449c22009b25c9e3281833eba81b957816d8a08f8503daa";
 }
